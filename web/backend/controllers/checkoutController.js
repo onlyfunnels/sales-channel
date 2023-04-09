565c269e734c8943018918855b76db8f5770acea
salmanhaseeb <salmanhaseeb@gmail.com>
2023-04-09 20:28:35 +0500

    cleaning the code of xontrollers

--- web/backend/controllers/checkoutController.js
+++ web/backend/controllers/checkoutController.js
@@ -13,99 +13,31 @@ export const getCheckoutById = asyncHandler(async (req, res) => {
 })
 
 export const createCheckout = asyncHandler(async (req, res) => {
-  // if(!req.body.text) {
-  //   res.status(400)
-  //   throw new Error('Please add a text field.')
-  // }
-
   const checkout = await checkoutHandler(req.body)
   res.status(200).json(JSON.parse(checkout.body))
-
-  // const goal = await Goal.create({
-  //   text: req.body.text
-  // })
-  // res.status(200).json(goal)
 })
 
 export const addLineItems = asyncHandler(async (req, res) => {
-  // const goal = await Goal.findById(req.params.id)
-
-  // if(!goal){
-  //   res.status(400)
-  //   throw new Error('Goal not found')
-  // }
-
-  console.log(req.body)
   const checkout = await addLineItemsHandler(req.body)
   res.status(200).json(JSON.parse(checkout.body))
-
-  // const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {new: true})
-  // res.status(200).json(updatedGoal)
 })
 
 export const updateLineItems = asyncHandler(async (req, res) => {
-  // const goal = await Goal.findById(req.params.id)
-
-  // if(!goal){
-  //   res.status(400)
-  //   throw new Error('Goal not found')
-  // }
-
-  console.log(req.body)
   const checkout = await updateLineItemsHandler(req.body)
   res.status(200).json(JSON.parse(checkout.body))
-
-  // const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {new: true})
-  // res.status(200).json(updatedGoal)
 })
 
 export const removeLineItems = asyncHandler(async (req, res) => {
-
   const checkout = await removeLineItemsHandler(req.body)
   res.status(200).json(JSON.parse(checkout.body))
-
-  // const goal = await Goal.findById(req.params.id)
-
-  // if(!goal){
-  //   res.status(400)
-  //   throw new Error('Goal not found')
-  // }
-
-  // // const deletedGoal = await Goal.findByIdAndDelete(req.params.id)
-  // // res.status(200).json(deletedGoal)
-
-  // await goal.remove()
-  // res.status(200).json({id: req.params.id})
 })
 
 export const applyDiscountCode = asyncHandler(async (req, res) => {
-  // const goal = await Goal.findById(req.params.id)
-
-  // if(!goal){
-  //   res.status(400)
-  //   throw new Error('Goal not found')
-  // }
-
-  console.log(req.body)
   const checkout = await applyDiscountCodeHandler(req.body)
   res.status(200).json(JSON.parse(checkout.body))
-
-  // const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {new: true})
-  // res.status(200).json(updatedGoal)
 })
 
 export const removeDiscountCode = asyncHandler(async (req, res) => {
-  // const goal = await Goal.findById(req.params.id)
-
-  // if(!goal){
-  //   res.status(400)
-  //   throw new Error('Goal not found')
-  // }
-
-  console.log(req.body)
   const checkout = await removeDiscountCodeHandler(req.body)
   res.status(200).json(JSON.parse(checkout.body))
-
-  // const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {new: true})
-  // res.status(200).json(updatedGoal)
 })
